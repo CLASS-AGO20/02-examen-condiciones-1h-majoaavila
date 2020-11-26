bcea24034122228ece4053eb991054b61da76b6c
Majo Ávila Amezcua <mavila7@ucol.mx>
2020-11-26 15:22:46 -0600

    implemented convertir()

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 export default class App {
 
+    convertir(pesos) {
+        let dolar;
+        dolar = 20.5 * pesos;
+
+        let euro;
+        euro = 22.7 * pesos;
+
+        if(pesos < 2000) {
+            return dolar;
+
+        } else if(pesos >= 2000) {
+            return euro;
+        }
+    }
 }
 
 let app = new App();
+
+console.log("Probando convertir()");
+console.log(app.convertir(1000));
+console.log(app.convertir(2000));
+console.log(app.convertir(3000));
+
