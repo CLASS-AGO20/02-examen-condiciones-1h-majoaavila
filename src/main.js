20ec469cc067f99e6c55a8b826332195637d0a5a
Majo Ávila Amezcua <mavila7@ucol.mx>
2020-11-26 15:45:01 -0600

    implemented costoRenta()

--- src/main.js
+++ src/main.js
@@ -14,6 +14,22 @@ export default class App {
             return euro;
         }
     }
+
+    costoRenta(kilometros) {
+        if (kilometros < 50) {
+            return kilometros * 3;
+
+        } else if(kilometros >= 50 && kilometros < 100) {
+            return kilometros * 5;
+
+        } else if(kilometros >= 100 && kilometros < 200) {
+            return kilometros * 6;
+
+        } else if(kilometros >= 200) {
+            return kilometros * 6.5;
+        }
+    }
+
 }
 
 let app = new App();
@@ -22,4 +38,17 @@ console.log("Probando convertir()");
 console.log(app.convertir(1000));
 console.log(app.convertir(2000));
 console.log(app.convertir(3000));
+
+console.log("Probando costoRenta()");
+console.log(app.convertir(30));
+console.log(app.convertir(50));
+console.log(app.convertir(60));
+console.log(app.convertir(99));
+console.log(app.convertir(100));
+console.log(app.convertir(150));
+console.log(app.convertir(199));
+console.log(app.convertir(200));
+console.log(app.convertir(300));
+
+
 
